2d064ddb83aa4af4fa2057eabd20bcc4d18911a7
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-09-15 19:53:45 -0300

    Adding entities medico and endereco

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,22 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+}
