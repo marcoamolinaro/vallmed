8d17ab919aeba055b9233cf502057eaf1785ca3d
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-09-19 17:22:13 -0300

    Move classes to domain package and added exception handler for EntityNotFoundException (404)

--- src/main/java/med/voll/api/repository/PacienteRepository.java
+++ src/main/java/med/voll/api/repository/PacienteRepository.java
@@ -1,6 +1,6 @@
 package med.voll.api.repository;
 
-import med.voll.api.paciente.Paciente;
+import med.voll.api.domain.paciente.Paciente;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
