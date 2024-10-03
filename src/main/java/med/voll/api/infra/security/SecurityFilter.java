7b8587cde29a3114f88c79a20340d39d1ab7c337
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-10-03 15:10:12 -0300

    Adding errors handlers

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -44,7 +44,7 @@ public class SecurityFilter extends OncePerRequestFilter {
         var authorizationHeader = request.getHeader("Authorization");
 
         if (authorizationHeader != null) {
-            return authorizationHeader.replace("Bearer ", "");
+            return authorizationHeader.replace("Bearer", "").trim();
         }
 
         return null;
